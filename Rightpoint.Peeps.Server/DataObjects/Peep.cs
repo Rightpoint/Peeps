0b3f3ad158c21d87b927edf1615fb6d6ad6f3be3
Brandon Barnett <BBarnett@rightpoint.com>
2015-08-11 21:12:43 -0500

    Data sync changes - saving data now

--- Rightpoint.Peeps.Server/DataObjects/Peep.cs
+++ Rightpoint.Peeps.Server/DataObjects/Peep.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.WindowsAzure.Mobile.Service;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.WindowsAzure.Mobile.Service;
 
 namespace Rightpoint.Peeps.Server.DataObjects
 {
     public class Peep : EntityData
     {
+        [Required, MaxLength(100)]
         public string Name { get; set; }
+
+        [Required, MaxLength(30)]
+        public string Team { get; set; }
+
+        [MaxLength(100)]
+        public string Hometown { get; set; }
+
+        public byte[] ImageBytes { get; set; }
+
+        [NotMapped]
+        public string ImagePath { get; set; }
     }
 }
--- Rightpoint.Peeps.Sync/Program.cs
+++ Rightpoint.Peeps.Sync/Program.cs
@@ -1,34 +1,84 @@
-﻿using System.Collections.Generic;
-using Rightpoint.Peeps.Sync.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using Rightpoint.Peeps.Server.DataObjects;
+using Rightpoint.Peeps.Server.Models;
+//using Rightpoint.Peeps.Sync.Models;
 
 namespace Rightpoint.Peeps.Sync
 {
     class Program
     {
+        private static readonly MobileServiceContext DbContext = new MobileServiceContext();
+
         static void Main(string[] args)
         {
+            var currentPeeps = DbContext.Peeps.AsEnumerable().ToList();
+
             List<Peep> peeps = new List<Peep>
             {
                 new Peep
                 {
-                    Name = "John Doe",
-                    ImagePath = @"c:\temp\peeps\person.bmp"
-                },
-                new Peep
-                {
-                    Name = "Jane Doe",
-                    ImagePath = @"c:\temp\peeps\person.bmp"
+                    Name = "Nick Groos",
+                    Hometown = "Grand Rapids, MI",
+                    Team = "App Dev",
+                    ImagePath = @"c:\temp\people\NickGroos.jpg"
                 }
             };
-
+            
             foreach (Peep peep in peeps)
             {
-                // load image to byte array
+                peep.ImageBytes = ImagePathToByteArray(peep.ImagePath);
 
-                // replace peep.ImageBytes with byte array
+                var currentPeep =
+                    currentPeeps.SingleOrDefault(
+                        p => String.Equals(peep.Name, p.Name, StringComparison.InvariantCultureIgnoreCase));
+
+                if (currentPeep == null)
+                {
+                    peep.Id = Guid.NewGuid().ToString();
+                    peep.CreatedAt = DateTime.UtcNow;
+                    peep.UpdatedAt = DateTime.UtcNow;
+                    peep.Deleted = false;
+                    
+                    DbContext.Peeps.Add(peep);
+                }
+                else
+                {
+                    currentPeep.Name = peep.Name;
+                    currentPeep.Hometown = peep.Hometown;
+                    currentPeep.Team = peep.Team;
+                    currentPeep.ImageBytes = peep.ImageBytes;
+
+                    currentPeep.UpdatedAt = DateTime.UtcNow;
+                }
             }
 
-            // sync with mobile service
+            try
+            {
+                DbContext.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                throw;
+            }
+        }
+
+        private static byte[] ImagePathToByteArray(string imagePath)
+        {
+            Image image = Image.FromFile(imagePath);
+            byte[] imageBytes;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, ImageFormat.Jpeg);
+                imageBytes = ms.ToArray();
+            }
+
+            return imageBytes;
         }
     }
 }
