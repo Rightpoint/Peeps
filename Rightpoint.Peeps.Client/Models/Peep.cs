12932086fd674e92519b469e593b46f63af667da
Brandon Barnett <BBarnett@rightpoint.com>
2015-08-11 19:06:18 -0500

    Cleanup

--- Rightpoint.Peeps.Client/Models/Peep.cs
+++ Rightpoint.Peeps.Client/Models/Peep.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rightpoint.Peeps.Client.Models
+﻿namespace Rightpoint.Peeps.Client.Models
 {
     public class Peep
     {
--- Rightpoint.Peeps.Sync/Models/Peep.cs
+++ Rightpoint.Peeps.Sync/Models/Peep.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rightpoint.Peeps.Sync.Models
+﻿namespace Rightpoint.Peeps.Sync.Models
 {
     public class Peep
     {
