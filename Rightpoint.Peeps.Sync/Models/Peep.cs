f82946b8f0b8e19fe1049f8c90b8028862d0525e
Brandon Barnett <BBarnett@rightpoint.com>
2015-08-11 19:02:36 -0500

    Creation of sync console app

---
+++ Rightpoint.Peeps.Sync/Models/Peep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rightpoint.Peeps.Sync.Models
+{
+    public class Peep
+    {
+        public string Name { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public byte[] ImageBytes { get; set; }
+    }
+}
---
+++ Rightpoint.Peeps.Sync/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Rightpoint.Peeps.Sync.Models;
+
+namespace Rightpoint.Peeps.Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Peep> peeps = new List<Peep>
+            {
+                new Peep
+                {
+                    Name = "John Doe",
+                    ImagePath = @"c:\temp\peeps\person.bmp"
+                },
+                new Peep
+                {
+                    Name = "Jane Doe",
+                    ImagePath = @"c:\temp\peeps\person.bmp"
+                }
+            };
+
+            foreach (Peep peep in peeps)
+            {
+                // load image to byte array
+
+                // replace peep.ImageBytes with byte array
+            }
+
+            // sync with mobile service
+        }
+    }
+}
