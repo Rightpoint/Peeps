7f48634a6fdb7a66c921707724d318838c6d855f
Joe Meyer <JMeyer@rightpoint.com>
2015-09-08 17:24:23 -0500

    Added Clock

---
+++ Rightpoint.Peeps.Client/BasicDigitalClock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using Rightpoint.Peeps.Client.Annotations;
+
+namespace Rightpoint.Peeps.Client
+{
+    public class BasicDigitalClock : ViewModelBase
+    {
+        private long currentTime;
+
+        public BasicDigitalClock()
+        {
+            UpdateCurrentTime();
+        }
+
+        private void UpdateCurrentTime()
+        {
+            currentTime = DateTime.UtcNow.Ticks;
+            RaisePropertyChanged(() => DateFormatted);
+            RaisePropertyChanged(() => TimeFormatted);
+        }
+
+        public string DateFormatted
+        {
+            get
+            {
+                UpdateCurrentTime();
+                DateTime now = new DateTime().AddTicks(currentTime);
+                string strDate = now.ToString("D");
+                return strDate;
+            }
+        }
+        public string TimeFormatted
+        {
+            get
+            {
+                UpdateCurrentTime();
+                DateTime now = new DateTime().AddTicks(currentTime);
+                string strTime = now.ToString("t");
+                return strTime;
+            }
+        }
+    }
+}
--- Rightpoint.Peeps.Client/ViewModels/MainViewModel.cs
+++ Rightpoint.Peeps.Client/ViewModels/MainViewModel.cs
@@ -5,7 +5,12 @@ using Microsoft.WindowsAzure.MobileServices;
 using Rightpoint.Peeps.Client.Models;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml;
+using GalaSoft.MvvmLight;
 
 namespace Rightpoint.Peeps.Client.ViewModels
 {
@@ -15,10 +20,32 @@ namespace Rightpoint.Peeps.Client.ViewModels
 
         public DynamicCollection<Peep> Peeps { get; set; } = new DynamicCollection<Peep>();
 
+        private BasicDigitalClock _digitalClock;
+
+        public BasicDigitalClock DigitalClock
+        {
+            get { return _digitalClock; }
+            set
+            {
+                if (_digitalClock != value)
+                {
+                    _digitalClock = value;
+                    RaisePropertyChanged(() => DigitalClock);
+                }
+            }
+        }
+
         public MainViewModel(NavigationService navigationService, IMobileServiceClient mobileServiceClient) : base(navigationService)
         {
             if (mobileServiceClient == null) throw new ArgumentNullException(nameof(mobileServiceClient));
             this._mobileServiceClient = mobileServiceClient;
+
+            DigitalClock = new BasicDigitalClock();
+
+            var dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            dispatcherTimer.Start();
         }
 
         protected override async Task LoadData(NavigationEventArgs e)
@@ -35,6 +62,14 @@ namespace Rightpoint.Peeps.Client.ViewModels
             this.Peeps.Cleanup();
 
             base.Cleanup();
+        }
+
+        private void dispatcherTimer_Tick(object sender, object e)
+        {
+            var strDate = DigitalClock.DateFormatted;
+            var strTime = DigitalClock.TimeFormatted;
+
+            Debug.WriteLine(strTime);
         }
     }
 }
