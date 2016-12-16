eee990d5f8dd69675d4680ca0d857309d9ea64eb
JMeyer <JMeyer@RPC-JMEYER512>
2016-12-16 09:54:28 -0600

    Peeps junk

--- Rightpoint.Peeps.Sync/Program.cs
+++ Rightpoint.Peeps.Sync/Program.cs
@@ -18,24 +18,170 @@ namespace Rightpoint.Peeps.Sync
         {
             var currentPeeps = DbContext.Peeps.AsEnumerable().ToList();
 
-            //TODO: we are abusing peeps by injecting events... a second model or refactor may be prudent
             List<Peep> peeps = new List<Peep>
             {
-                //new Peep
-                //{
-                //    Name = "Event_20160604",
-                //    Hometown ="Event_20160604",
-                //    Team ="Event_20160604",
-                //    Office ="Event_20160604",
-                //    ImagePath = @"c:\temp\people\June_Events-01-1.jpg"
-                //},
                 new Peep
                 {
-                    Name = "Dani Soukup",
-                    Hometown = "Svilengrad, Bulgaria",
+                    Name = "Jason Alexander",
+                    Hometown = "",
+                    Team = "Managed Services",
+                    Office = "Chicago",
+                    ImagePath = @"c:\temp\people\Jason Alexander.jpg"
+                },
+                new Peep
+                {
+                    Name = "Gautam Budidha",
+                    Hometown = "",
+                    Team = "Managed Services",
+                    Office = "Chicago",
+                    ImagePath = @"c:\temp\people\Gautam Budidha.jpg"
+                },
+                new Peep
+                {
+                    Name = "Jack Davidson",
+                    Hometown = "",
+                    Team = "Account Management",
+                    Office = "Chicago",
+                    ImagePath = @"c:\temp\people\Jack Davidson.jpeg"
+                },
+                new Peep
+                {
+                    Name = "Rebe De La Paza",
+                    Hometown = "",
+                    Team = "Salesforce",
+                    Office = "Chicago",
+                    ImagePath = @"c:\temp\people\Rebe De La Paza.jpg"
+                },
+                new Peep
+                {
+                    Name = "Sruti Dhulipala",
+                    Hometown = "",
+                    Team = "Design & Strategy",
+                    Office = "Los Angeles",
+                    ImagePath = @"c:\temp\people\Sruti Dhulipala.jpg"
+                },
+
+                new Peep
+                {
+                    Name = "Sarah Doppelberger",
+                    Hometown = "",
+                    Team = "Alliances",
+                    Office = "detroit",
+                    ImagePath = @"c:\temp\people\Sarah Doppelberger.jpg"
+                },
+                new Peep
+                {
+                    Name = "Rizvi Hameed",
+                    Hometown = "",
+                    Team = "CMS",
+                    Office = "Boston",
+                    ImagePath = @"c:\temp\people\Rizvi Hameed.jpg"
+                },
+                new Peep
+                {
+                    Name = "Joe Hans",
+                    Hometown = "",
+                    Team = "App Dev",
+                    Office = "Atlanta",
+                    ImagePath = @"c:\temp\people\Joe Hans.jpg"
+                },
+                new Peep
+                {
+                    Name = "Nancy Hoag",
+                    Hometown = "",
+                    Team = "PMO",
+                    Office = "Dallas",
+                    ImagePath = @"c:\temp\people\Nancy Hoag.jpg"
+                },
+                new Peep
+                {
+                    Name = "Lew Jones",
+                    Hometown = "",
+                    Team = "Alliances",
+                    Office = "Chicago",
+                    ImagePath = @"c:\temp\people\Lewis Jones.png"
+                },
+
+                new Peep
+                {
+                    Name = "Kevin Joyce ",
+                    Hometown = "",
+                    Team = "PMO",
+                    Office = "Chicago",
+                    ImagePath = @"c:\temp\people\Kevin Joyce.jpg"
+                },
+                new Peep
+                {
+                    Name = "Jess Lewis",
+                    Hometown = "",
+                    Team = "QA",
+                    Office = "Atlanta",
+                    ImagePath = @"c:\temp\people\Jessica Lewis.jpg"
+                },
+                new Peep
+                {
+                    Name = "Jeff O'Neill",
+                    Hometown = "",
+                    Team = "Strategy",
+                    Office = "Boston",
+                    ImagePath = @"c:\temp\people\Jeff O'Neill.jpg"
+                },
+                new Peep
+                {
+                    Name = "Tom Quish",
+                    Hometown = "",
                     Team = "Creative",
                     Office = "Chicago",
-                    ImagePath = @"c:\temp\people\Dani Soukup.jpg"
+                    ImagePath = @"c:\temp\people\Tom Quish.jpeg"
+                },
+                new Peep
+                {
+                    Name = "Melissa Ranalli",
+                    Hometown = "",
+                    Team = "PMO",
+                    Office = "Boston",
+                    ImagePath = @"c:\temp\people\Melissa Ranalli.jpg"
+                },
+
+                new Peep
+                {
+                    Name = "Camille Sharrow",
+                    Hometown = "",
+                    Team = "Account Manager",
+                    Office = "Chicago",
+                    ImagePath = @"c:\temp\people\Camille Sharrow.jpg"
+                },
+                new Peep
+                {
+                    Name = "Delia Starr",
+                    Hometown = "",
+                    Team = "People Potential",
+                    Office = "chicago",
+                    ImagePath = @"c:\temp\people\Delia Starr.jpg"
+                },
+                new Peep
+                {
+                    Name = "Leigh Vitek",
+                    Hometown = "",
+                    Team = "Account Manager",
+                    Office = "Chicago",
+                    ImagePath = @"c:\temp\people\Leigh Vitek.jpg"
+                },
+                new Peep
+                {
+                    Name = "Mariusz Walczuk",
+                    Hometown = "",
+                    Team = "Delivery Support",
+                    Office = "Chicago",
+                    ImagePath = @"c:\temp\people\Mariusz Walczuk.png"
+                },
+                new Peep
+                {
+                    Name = "Ming Zhong",
+                    Hometown = "",
+                    Team = "App Dev",
+                    Office = "Chicago",
+                    ImagePath = @"c:\temp\people\Minghua Zhong.jpg"
                 },
                 /*
                 new Peep
